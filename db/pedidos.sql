b41629b1f6108495f10366daa8b879ae6d04ce9b
dextter1913 <dextter1913@gmail.com>
2021-10-26 20:31:32 -0500

    Agregando las demas tablas

--- db/pedidos.sql
+++ db/pedidos.sql
@@ -1,7 +1,8 @@
-DROP IF EXISTS pedidos;
+DROP DATABASE IF EXISTS pedidos;
+
+CREATE DATABASE IF NOT EXISTS pedidos;
 
 USE pedidos;
-CREATE DATABASE IF NOT EXISTS pedidos;
 
 CREATE TABLE usuarios(
     idUsuario BIGINT PRIMARY KEY AUTO_INCREMENT,
@@ -16,4 +17,48 @@ CREATE TABLE usuarios(
     fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     estado VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE productos(
+    idProducto BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255) NOT NULL,
+    precio DOUBLE NOT NULL,
+    iva DOUBLE NOT NULL,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estado VARCHAR(255) NOT NULL,
+    idUsuario BIGINT NOT NULL,
+    INDEX(idUsuario),
+    FOREIGN KEY(idUsuario) REFERENCES usuarios(idUsuario)
+);
+
+CREATE TABLE pedidosEncabezado(
+    idPedidoEncabezado BIGINT PRIMARY KEY AUTO_INCREMENT,
+    fecha_pedido DATE NOT NULL,
+    fecha_entrega DATE NOT NULL,
+    fecha_entregado DATE NOT NULL,
+    fecha_cancelado DATE NOT NULL,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estado VARCHAR(255) NOT NULL,
+    idUsuario BIGINT NOT NULL,
+    INDEX(idUsuario),
+    FOREIGN KEY(idUsuario) REFERENCES usuarios(idUsuario)
+);
+
+CREATE TABLE pedidoCuerpo(
+    idPedidoCuerpo BIGINT PRIMARY KEY AUTO_INCREMENT,
+    cantidad INT NOT NULL,
+    precio DOUBLE NOT NULL,
+    iva DOUBLE NOT NULL,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    estado VARCHAR(255) NOT NULL,
+    idPedidoEncabezado BIGINT NOT NULL,
+    idProducto BIGINT NOT NULL,
+    INDEX(idPedidoEncabezado),
+    INDEX(idProducto),
+    FOREIGN KEY(idPedidoEncabezado) REFERENCES pedidosEncabezado(idPedidoEncabezado),
+    FOREIGN KEY(idProducto) REFERENCES productos(idProducto)
 );
